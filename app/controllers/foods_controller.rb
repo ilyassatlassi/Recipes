7f363cb3da761884e50acc6616c2c704d1ca36b8
ilyass atlassi <atlassiilyass@gmail.com>
2023-07-03 18:41:39 +0100

    add function to controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -3,66 +3,33 @@ class FoodsController < ApplicationController
 
   # GET /foods or /foods.json
   def index
-    @foods = Food.all
-  end
-
-  # GET /foods/1 or /foods/1.json
-  def show
+    @foods = current_user.foods.includes(:user)
   end
 
   # GET /foods/new
   def new
-    @food = Food.new
-  end
-
-  # GET /foods/1/edit
-  def edit
+    @food = current_user.foods.build
   end
 
   # POST /foods or /foods.json
   def create
-    @food = Food.new(food_params)
-
-    respond_to do |format|
-      if @food.save
-        format.html { redirect_to food_url(@food), notice: "Food was successfully created." }
-        format.json { render :show, status: :created, location: @food }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @food.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /foods/1 or /foods/1.json
-  def update
-    respond_to do |format|
-      if @food.update(food_params)
-        format.html { redirect_to food_url(@food), notice: "Food was successfully updated." }
-        format.json { render :show, status: :ok, location: @food }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @food.errors, status: :unprocessable_entity }
-      end
+    @food = current_user.foods.build(food_params)
+    if @food.save
+      redirect_to foods_path, notice: 'Food was successfully created.'
+    else
+      render :new
     end
   end
 
   # DELETE /foods/1 or /foods/1.json
   def destroy
+    @food = current_user.foods.find(params[:id])
     @food.destroy
-
-    respond_to do |format|
-      format.html { redirect_to foods_url, notice: "Food was successfully destroyed." }
-      format.json { head :no_content }
+    redirect_to foods_path, notice: 'Food item was successfully deleted.'
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_food
-      @food = Food.find(params[:id])
-    end
-
     # Only allow a list of trusted parameters through.
     def food_params
       params.require(:food).permit(:name, :measurement_unit, :price, :quantity, :user_id)
