14251e598c7e109da82a1ee9d53c9a2bb16073bb
ilyass atlassi <atlassiilyass@gmail.com>
2023-07-03 18:21:24 +0100

    arelations beetween user and food

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "foods#index"
   # get '/devise/registrations'
 end
